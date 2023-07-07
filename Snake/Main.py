7d23cc4fa14e7bd7efbf2a71a28bafaa72e4db30
GDcheerios <minecreeper0913@gmail.com>
2023-07-06 21:55:25 -0500

    cleaned up code

--- Snake/Main.py
+++ Snake/Main.py
@@ -11,11 +11,8 @@ global height
 width = 400
 height = 400
 
-
-
 window = pygame.display.set_mode((width, height))
 snake_speed = 1
-
 
 pygame.display.set_caption("Snake")
 background_color = (46, 153, 37)  # White color
@@ -23,21 +20,18 @@ snake_color = (0, 0, 0)  # White color
 
 
 class snake():
-    
+
     def moveSnake(snake_list):
         for p in snake_list:
-            p.move(up,down,left,right)
-        
-            
-
+            p.move(up, down, left, right)
 
     def printSnake(snake_list):
         for p in snake_list:
-            pygame.draw.rect(window,snake_color,p.getRect())
+            pygame.draw.rect(window, snake_color, p.getRect())
 
 
-head = pee(width/2, height/2)
-snake_list =[head]
+head = pee(width / 2, height / 2)
+snake_list = [head]
 while running:
     left = False
     right = False
@@ -51,14 +45,14 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        up = True  
+        up = True
     elif keys[pygame.K_s]:
-        down= True
+        down = True
     elif keys[pygame.K_a]:
         left = True
     elif keys[pygame.K_d]:
         right = True
-   
+
     window.fill(background_color)
     snake.moveSnake(snake_list)
     snake.printSnake(snake_list)
--- Snake/Peice.py
+++ Snake/Peice.py
@@ -1,39 +1,34 @@
-xpos=0
-ypos=0
-size =0
+xpos = 0
+ypos = 0
+size = 0
 import pygame
 
 
 class pee():
 
-    def __init__(self,x,y):
-     self.xpos = x
-     self.ypos =y
-     self.size = 20
-
+    def __init__(self, x, y):
+        self.xpos = x
+        self.ypos = y
+        self.size = 20
 
     def getX(self):
-
         return xpos
-    
+
     def getY(self):
         return ypos
-    
+
     def getSize(self):
-       return size
-    
+        return size
+
     def getRect(self):
-         return pygame.Rect(self.xpos,ypos,size,size)
-   
-    
-    def move(self,up,down,left,right):
-        if(up):
-           ypos=ypos+1
-        elif(down):
-           ypos = ypos-1
-        elif(left):
-           xpos = xpos +1
-        elif(right):
-           xpos = xpos -1
-           
-    
+        return pygame.Rect(self.xpos, ypos, size, size)
+
+    def move(self, up, down, left, right):
+        if (up):
+            self.ypos += 1
+        elif (down):
+            self.ypos += 1
+        elif (left):
+            self.xpos += 1
+        elif (right):
+            self.xpos += 1
